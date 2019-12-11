b7baf75316c1eaf7eebd965a0ab250414a906bb9
Wachiraphong Prasoetwong <wachiraphong.p@kbtg.tech>
2019-12-11 11:33:44 +0700

    [CV Scanner] fix cannot run

--- app/src/main/java/com/wachirapong/testdocumentscan/MainActivity.kt
+++ app/src/main/java/com/wachirapong/testdocumentscan/MainActivity.kt
@@ -2,11 +2,13 @@ package com.wachirapong.testdocumentscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wachirapong.cvscanner.CVScanner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CVScanner.startScanner(this, false, 9002)
     }
 }
