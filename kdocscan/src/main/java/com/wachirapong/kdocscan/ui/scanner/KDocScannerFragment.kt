c3043d1db23a4b1438437f3a08d529fdfc1d6363
Wachiraphong Prasoetwong <wachiraphong.p@kbtg.tech>
2019-12-12 11:48:49 +0700

    finish getQuadrilateral

--- kdocscan/src/main/java/com/wachirapong/kdocscan/ui/scanner/KDocScannerFragment.kt
+++ kdocscan/src/main/java/com/wachirapong/kdocscan/ui/scanner/KDocScannerFragment.kt
@@ -14,14 +14,16 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 import com.wachirapong.kdocscan.R
+import com.wachirapong.kdocscan.data.Quadrilateral
 import com.wachirapong.kdocscan.ui.BaseFragment
 import com.wachirapong.kdocscan.util.ImageUtil
 import kotlinx.android.synthetic.main.fragment_kdoc_scanner.*
 import org.opencv.android.Utils
-import org.opencv.core.CvType
-import org.opencv.core.Mat
-import org.opencv.core.Size
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 
 class KDocScannerFragment : BaseFragment() {
@@ -66,6 +68,8 @@ class KDocScannerFragment : BaseFragment() {
         }
     }
 
+    private var size: Size? = null
+
     private fun getImageAnalysis(context: Context): ImageAnalysis {
         return ImageAnalysis.Builder()
             .setBackpressureStrategy(ImageAnalysis.BackpressureStrategy.BLOCK_PRODUCER)
@@ -76,19 +80,31 @@ class KDocScannerFragment : BaseFragment() {
                         val original = ImageUtil.imageToBitmap(image.image!!, rotationDegrees.toFloat())
 
                         // bitmap to mat
-                        val size = Size(image.width.toDouble(), image.height.toDouble())
+                        size = Size(image.width.toDouble(), image.height.toDouble())
                         val originalMat = Mat(size, CvType.CV_8UC4)
                         val bmp32 = original.copy(Bitmap.Config.ARGB_8888, true)
                         Utils.bitmapToMat(bmp32, originalMat)
+                        val grayScale = Mat(size, CvType.CV_8UC4)
+                        val gaussianBlur = Mat(size, CvType.CV_8UC4)
 
                         // convert the image to grayscale, blur it, and find edges
                         // in the image
                         val edged = Mat(size, CvType.CV_8UC1)
-                        Imgproc.cvtColor(originalMat, originalMat, Imgproc.COLOR_RGBA2GRAY)
-                        Imgproc.GaussianBlur(originalMat, originalMat, Size(5.0, 5.0), 0.0)
+                        Imgproc.cvtColor(originalMat, grayScale, Imgproc.COLOR_RGBA2GRAY)
+                        Imgproc.GaussianBlur(grayScale, gaussianBlur, Size(5.0, 5.0), 0.0)
                         Imgproc.Canny(originalMat, edged, 75.0, 200.0)
                         Utils.matToBitmap(edged, bmp32)
-                        findContour()
+
+                        val contour: ArrayList<MatOfPoint> = findContour(edged)
+
+                        val a = getQuadrilateral(contour)
+
+                        val imageWithContour = Mat(size, CvType.CV_8UC4)
+                        originalMat.copyTo(imageWithContour)
+                        if (a != null) {
+                            Imgproc.drawContours(imageWithContour, contour, -1, Scalar(0.0, 255.0, 0.0), 2)
+                        }
+                        Utils.matToBitmap(imageWithContour, bmp32)
                         // END
                         (context as Activity).runOnUiThread {
                             imageView.setImageBitmap(bmp32)
@@ -99,8 +115,48 @@ class KDocScannerFragment : BaseFragment() {
             }
     }
 
-    private fun findContour(edged: Mat) {
-        Imgproc.findContours()
+    private fun findContour(edged: Mat): ArrayList<MatOfPoint> {
+        // # find the contours in the edged image, keeping only the
+        // # largest ones, and initialize the screen contour
+        val copyEdged = Mat(size, CvType.CV_8UC4)
+        edged.copyTo(copyEdged)
+        val contours = ArrayList<MatOfPoint>()
+        val hierarchy = Mat()
+        Imgproc.findContours(copyEdged, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE)
+        hierarchy.release()
+        contours.sortWith(Comparator { lhs: MatOfPoint, rhs: MatOfPoint ->
+            Imgproc.contourArea(rhs).compareTo(Imgproc.contourArea(lhs))
+        })
+        copyEdged.release()
+        return contours
+    }
+
+    private fun getQuadrilateral(contours: ArrayList<MatOfPoint>): Mat? {
+        // loop over the contours
+        for (c in contours) {
+            // approximate the contour
+            val c2f = MatOfPoint2f()
+            c.convertTo(c2f, CvType.CV_32FC2)
+            val peri = Imgproc.arcLength(c2f, true)
+            val approx = MatOfPoint2f()
+            Imgproc.approxPolyDP(c2f, approx, 0.02 * peri, true)
+
+            val points = approx.toList()
+
+            // if our approximated contour has four points, then we
+            // can assume that we have found our screen
+            // select biggest 4 angles polygon
+            if (points.size == 4) {
+
+//                val foundPoints = sortPoints(points)
+//
+//                if (insideArea(foundPoints, size)) {
+//                    return Quadrilateral(c, foundPoints)
+//                }
+                return approx
+            }
+        }
+        return null
     }
 
 }
