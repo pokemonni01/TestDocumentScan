add90595ab22aa7b7324aa2a73772723f1e89ac5
kanok.k <kanok.k@kbtg.tech>
2019-12-19 14:06:46 +0700

    commit reviewDoc

--- kdocscan/src/main/java/com/wachirapong/kdocscan/ui/testscanner/KDocScannerActivity.kt
+++ kdocscan/src/main/java/com/wachirapong/kdocscan/ui/testscanner/KDocScannerActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
 import com.wachirapong.kdocscan.R
+import com.wachirapong.kdocscan.ui.reviewdocument.ReviewDocFragment
 import com.wachirapong.kdocscan.ui.scanner.ScannerFragment
 
 class KDocScannerActivity : AppCompatActivity() {
@@ -18,12 +21,20 @@ class KDocScannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kdoc_scanner)
+
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS+"/testDoc.jpg")
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container,
                 ScannerFragment.initInstance()
+                //ReviewDocFragment.initInstance(path.absolutePath)
             )
             .addToBackStack(null)
             .commit()
+    }
+
+    fun onReviewClickBack() {
+        Toast.makeText(this,"back", Toast.LENGTH_LONG).show()
     }
 }
