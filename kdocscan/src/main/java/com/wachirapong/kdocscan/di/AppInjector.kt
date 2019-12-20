951f66a91972c4c1ba7c36262b63b4c5e51f2e02
Wachiraphong Prasoetwong <wachiraphong.p@kbtg.tech>
2019-12-20 11:29:03 +0700

    finish file manager

--- kdocscan/src/main/java/com/wachirapong/kdocscan/di/AppInjector.kt
+++ kdocscan/src/main/java/com/wachirapong/kdocscan/di/AppInjector.kt
@@ -1,13 +1,20 @@
 package com.wachirapong.kdocscan.di
 
+import com.wachirapong.kdocscan.manager.FileManager
+import com.wachirapong.kdocscan.manager.FileManagerImpl
 import com.wachirapong.kdocscan.ui.scanner.ScannerContract
 import com.wachirapong.kdocscan.ui.scanner.ScannerPresenter
 import com.wachirapong.kdocscan.util.ImageProcessor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+
+private val managerModule = module {
+    factory<FileManager> { FileManagerImpl(androidContext()) }
+}
 
 private val documentScanModule = module {
     factory { ImageProcessor() }
     factory<ScannerContract.Presenter> { ScannerPresenter() }
 }
 
-val modules = listOf(documentScanModule)
+val modules = listOf(managerModule, documentScanModule)
